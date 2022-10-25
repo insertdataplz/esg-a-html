52b997c773ad72140ddf6a871e75b637a7683334
insertdataplz <pipin4682@gmail.com>
2022-10-25 21:53:46 +0900

    2022-10-25 과제

--- add_two_number.js
+++ add_two_number.js
@@ -3,5 +3,5 @@ function doSomething(){
     let b = document.getElementById('inputB').value;
     document.getElementById("valueA").innerHTML = a;
     document.getElementById("valueB").innerHTML = b;
-    document.getElementById("valueC").innerHTML = Number(a) + Number(b);
+    document.getElementById("valueC").innerHTML = Number(a)+Number(b);
 }
